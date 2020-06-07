eb137dd6f53416191253881b1aee7d7e9a39910d
Jin Cheng <jinchengjoker@foxmail.com>
2020-06-07 16:56:00 +0800

    优化代码

--- api.js
+++ api.js
@@ -17,79 +17,87 @@ module.exports = {
 
   list: async () => {
     const taskList = await db.read()
-    inquirer
-      .prompt({
-        type: 'list',
-        name: 'taskIndex',
-        message: '请选择操作/任务',
-        choices: [
-          {name: '退出', value: -1},
-          ...taskList.map((task, index) => {
-            return {
-              name: `${task.done ? '[√]' : '[_]'} ${index + 1}. ${task.taskName}`,
-              value: index
-            }
-          }),
-          {name: '+ 创建新任务', value: -2}
-        ]
-      })
-      .then(answer => {
-        const {taskIndex} = answer
-        if (taskIndex >= 0) {
-          inquirer
-            .prompt([
-              {
-                type: 'list',
-                name: 'action',
-                message: '请选择操作',
-                choices: [
-                  {name: '退出', value: 'quit'},
-                  {name: '已完成', value: 'done'},
-                  {name: '未完成', value: 'undone'},
-                  {name: '编辑', value: 'edit'},
-                  {name: '删除', value: 'remove'},
-                ]
-              }
-            ])
-            .then(answer2 => {
-              switch (answer2.action) {
-                case 'done':
-                  taskList[taskIndex].done = true
-                  db.write(taskList)
-                  break
-                case 'undone':
-                  taskList[taskIndex].done = false
-                  db.write(taskList)
-                  break
-                case 'edit':
-                  inquirer.prompt({
-                    type: 'input',
-                    name: 'taskName',
-                    message: "请输入新的任务名"
-                  }).then(answer3 => {
-                    taskList[taskIndex].taskName = answer3.taskName
-                    db.write(taskList)
-                  })
-                  break
-                case 'remove':
-                  taskIndex.splice(taskIndex, 1)
-                  db.write(taskList)
-                  break
-              }
-            })
-        } else if (taskIndex === -2) {
-          inquirer.prompt({
-            type: 'input',
-            name: 'taskName',
-            message: "请输入任务名"
-          }).then(answer4 => {
-            taskList.push({
-              taskName: answer4.taskName,
-              done: false
-            })
-            db.write(taskList)
-          })
-        }
-      });
+    listAllTasks(taskList).then(answer => handleUserSelect(taskList, answer))
   },
+}
+
+function listAllTasks(taskList) {
+  return inquirer.prompt({
+    type: 'list',
+    name: 'taskIndex',
+    message: '请选择操作/任务',
+    choices: [
+      {name: '退出', value: -1},
+      ...taskList.map((task, index) => {
+        return {
+          name: `${task.done ? '[√]' : '[_]'} ${index + 1}. ${task.taskName}`,
+          value: index
+        }
+      }),
+      {name: '+ 创建新任务', value: -2}
+    ]
+  })
+}
+
+function handleUserSelect(taskList, answer) {
+  const {taskIndex} = answer
+  if (taskIndex >= 0) {
+    // 选择了某个任务
+    listTaskActions().then(answer => handleTaskAction(taskList, taskIndex, answer))
+  } else if (taskIndex === -2) {
+    // 选择了创建新任务
+    askTaskName().then(answer => {
+      taskList.push({
+        taskName: answer.taskName,
+        done: false
+      })
+      db.write(taskList)
+    })
+  }
+}
+
+function listTaskActions() {
+  return inquirer.prompt({
+    type: 'list',
+    name: 'action',
+    message: '请选择操作',
+    choices: [
+      {name: '退出', value: 'quit'},
+      {name: '已完成', value: 'done'},
+      {name: '未完成', value: 'undone'},
+      {name: '编辑', value: 'edit'},
+      {name: '删除', value: 'remove'},
+    ]
+  })
+}
+
+function handleTaskAction(taskList, taskIndex, answer) {
+  switch (answer.action) {
+    case 'done':
+      taskList[taskIndex].done = true
+      db.write(taskList)
+      break
+    case 'undone':
+      taskList[taskIndex].done = false
+      db.write(taskList)
+      break
+    case 'edit':
+      askTaskName().then(answer => {
+        taskList[taskIndex].taskName = answer.taskName
+        db.write(taskList)
+      })
+      break
+    case 'remove':
+      taskList.splice(taskIndex, 1)
+      db.write(taskList)
+      break
+  }
+}
+
+function askTaskName() {
+  return inquirer.prompt({
+    type: 'input',
+    name: 'taskName',
+    message: "请输入任务名"
+  })
 }
