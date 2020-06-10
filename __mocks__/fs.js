2a7f70c0f6f6c5a45d844222162021eca6c09535
JinCheng <jinchengjoker@foxmail.com>
2020-06-10 15:35:48 +0800

    添加 db.write 单元测试

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,13 +1,22 @@
 const fs = jest.genMockFromModule('fs');
 
-let mocks
+let readMock
+let writeMock
 
-fs.setMock = (err, data) => {
-  mocks = [err, data]
+fs.setReadMock = (err, data) => {
+  readMock = [err, data]
+}
+fs.setWriteMock = (fn) => {
+  writeMock = fn
 }
 
 fs.readFile = (path, options, callback) => {
-  callback(...mocks)
+  if(callback === undefined) callback = options
+  callback(...readMock)
+}
+fs.writeFile = (path, data, options, callback) => {
+  if(callback === undefined) callback = options
+  writeMock(data, callback)
 }
 
 module.exports = fs
--- __tests__/db.test.js
+++ __tests__/db.test.js
@@ -7,9 +7,20 @@ describe('db', () => {
 
   it('read', async () => {
     const temp = [{taskName: 'buy xxx', done: true}]
-    fs.setMock(null, JSON.stringify(temp))
+    fs.setReadMock(null, JSON.stringify(temp))
     const data = await db.read()
     expect(data).toStrictEqual(temp)
+  })
+
+  it('write', async () => {
+    let fake
+    fs.setWriteMock((data, callback) => {
+      fake = data
+      callback(null)
+    })
+    const temp = [{taskName: 'buy xxx', done: true}]
+    await db.write(temp)
+    expect(fake).toBe(JSON.stringify(temp) + '\n')
   })
 
 })
